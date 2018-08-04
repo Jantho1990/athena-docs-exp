5cf0bf611a0d7ae039552285bc9365384a20da69
Joshua Anthony <jantho1990@gmail.com>
2018-08-04 17:14:07 -0500

    added NotePage; refactored existing code to link to NotePage for viewing individual notes; added route for individual notes

---
+++ src/components/pages/NotePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="codex-page">
+    <NoteView v-bind="note"/>
+  </div>
+</template>
+
+<script>
+import content from '../../content/demoData.json'
+import NoteView from '../NoteView'
+
+export default {
+  components: {
+    NoteView
+  },
+  computed: {
+    note () {
+      // TODO: Figure out why this needs to be a computed
+      return content.notes.filter(note => note.id === this.id)
+    }
+  },
+  data () {
+    return {
+      id: Number(this.$route.params.id)
+    }
+  },
+  methods: {
+    assignNotes () {}
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -3,13 +3,15 @@ import VueRouter from 'vue-router'
 import CodicesPage from './components/pages/CodicesPage'
 import CodexPage from './components/pages/CodexPage'
 import NotesPage from './components/pages/NotesPage'
+import NotePage from './components/pages/NotePage'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/codices', component: CodicesPage },
   { path: '/notes', component: NotesPage },
-  { path: '/codex/:id', component: CodexPage }
+  { path: '/codex/:id', component: CodexPage },
+  { path: '/notes/:id', component: NotePage }
 ]
 
 const router = new VueRouter({
