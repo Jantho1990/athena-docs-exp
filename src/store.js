e53333b7171ecd31eae71b247eecb904a544e29a
Josh Anthony <janthony@brokerbin.com>
2018-08-07 12:32:07 -0500

    added getters to store

--- src/store.js
+++ src/store.js
@@ -7,6 +7,19 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     ...demoData
+  },
+  getters: {
+    codices: state => state.codices.map(codex => {
+      return {
+        ...codex,
+        notes: codex.notes
+          .map(noteId => {
+            return state.notes.find(note => note.id === noteId)
+          })
+          .filter(note => note !== undefined)
+      }
+    }),
+    notes: state => state.notes
   }
 })
 
