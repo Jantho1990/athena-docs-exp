97dce70bf36c6e813298a360db6bfe4a6f67582e
Josh Anthony <janthony@brokerbin.com>
2018-08-28 13:00:51 -0500

    added attach/detach mutations for codex and notes; tried refactoring existing Vuex code to use the attach/detach mutations but it is currently bugged

--- src/store.js
+++ src/store.js
@@ -9,9 +9,13 @@ const INCREMENT_ID = 'INCREMENT_ID'
 const STORE_CODEX = 'STORE_CODEX'
 const UPDATE_CODEX = 'UPDATE_CODEX'
 const DESTROY_CODEX = 'DESTROY_CODEX'
+const ATTACH_CODEX = 'ATTACH_CODEX'
+const DETACH_CODEX = 'DETACH_CODEX'
 const STORE_NOTE = 'STORE_NOTE'
 const UPDATE_NOTE = 'UPDATE_NOTE'
 const DESTROY_NOTE = 'DESTROY_NOTE'
+const ATTACH_NOTE = 'ATTACH_NOTE'
+const DETACH_NOTE = 'DETACH_NOTE'
 
 // Actions
 export const ADD_CODEX = 'ADD_CODEX'
@@ -68,16 +72,16 @@ const store = new Vuex.Store({
 
       state.codices = codices
 
-      codex.notes.forEach(noteId => {
+      /* codex.notes.forEach(noteId => {
         const n = state.notes.findIndex(nt => nt.id === noteId)
         state.notes[n] = {
           ...state.notes[n],
-          notes: [
-            ...state.notes[n].notes,
+          codices: [
+            ...state.notes[n].codices,
             codexEntry.id
           ]
         }
-      })
+      }) */
     },
     [UPDATE_CODEX] (state, payload) {
       const { codex } = payload
@@ -89,23 +93,49 @@ const store = new Vuex.Store({
         ...codex
       })
 
-      if (codex.notes) {
+      /* if (codex.notes) {
         codex.notes.forEach(noteId => {
           const n = state.notes.findIndex(nt => nt.id === noteId)
           state.notes[n] = {
             ...state.notes[n],
-            notes: [
-              ...state.notes[n].notes,
+            codices: [
+              ...state.notes[n].codices,
               codex.id
             ]
           }
         })
-      }
+      } */
     },
     [DESTROY_CODEX] (state, payload) {
       const { id } = payload
 
       state.codices = state.codices.filter(codex => codex.id !== id)
+
+      state.notes = state.notes.map(note => {
+        return {
+          ...note,
+          codices: note.codices ? note.codices.filter(c => c !== id) : []
+        }
+      })
+    },
+    [ATTACH_CODEX] (state, payload) {
+      const { codex, codex: { id } } = payload
+
+      codex.notes.forEach(noteId => {
+        const n = state.notes.findIndex(nt => nt.id === noteId)
+        if (state.notes[n].codices.findIndex(codexId => codexId === id) !== -1) {
+          state.notes[n] = {
+            ...state.notes[n],
+            codices: [
+              ...state.notes[n].codices,
+              id
+            ]
+          }
+        }
+      })
+    },
+    [DETACH_CODEX] (state, payload) {
+      const { id } = payload
 
       state.notes = state.notes.map(note => {
         return {
@@ -129,7 +159,7 @@ const store = new Vuex.Store({
 
       state.notes = notes
 
-      note.codices.forEach(codexId => {
+      /* note.codices.forEach(codexId => {
         const c = state.codices.findIndex(cdx => cdx.id === codexId)
         state.codices[c] = {
           ...state.codices[c],
@@ -138,7 +168,7 @@ const store = new Vuex.Store({
             noteEntry.id
           ]
         }
-      })
+      }) */
     },
     [UPDATE_NOTE] (state, payload) {
       const { note } = payload
@@ -150,7 +180,7 @@ const store = new Vuex.Store({
         ...note
       })
 
-      if (note.codices) {
+      /* if (note.codices) {
         note.codicies.forEach(codexId => {
           const c = state.codices.findIndex(ct => ct.id === codexId)
           state.codices[c] = {
@@ -161,17 +191,43 @@ const store = new Vuex.Store({
             ]
           }
         })
-      }
+      } */
     },
     [DESTROY_NOTE] (state, payload) {
       const { id } = payload
 
       state.notes = state.notes.filter(note => note.id !== id)
 
-      state.codices = state.codices.map(codex => {
+      /* state.codices = state.codices.map(codex => {
         return {
           ...codex,
           notes: codex.notes ? codex.notes.filter(n => n !== id) : []
+        }
+      }) */
+    },
+    [ATTACH_NOTE](state, payload) {
+      const { note, note: { id } } = payload
+
+      note.codices.forEach(codexId => {
+        const c = state.codices.findIndex(ct => ct.id === codexId)
+        if (state.codices[c].notes.findIndex(noteId => noteId === id) !== -1) {
+          state.codices[c] = {
+            ...state.codices[c],
+            notes: [
+              ...state.codices[c].notes,
+              id
+            ]
+          }
+        }
+      })
+    },
+    [DETACH_NOTE](state, payload) {
+      const { id } = payload
+
+      state.codices = state.codices.map(codex => {
+        return {
+          ...codex,
+          notes: codex.notes ? codex.notes.filter(c => c !== id) : []
         }
       })
     }
@@ -180,37 +236,43 @@ const store = new Vuex.Store({
     [ADD_CODEX] ({ commit }, payload) {
       const { codex } = payload
       if (codex) {
-        commit(STORE_CODEX, { codex: {...codex} })
+        commit(STORE_CODEX, { codex: { ...codex } })
+        commit(ATTACH_CODEX, { codex: { ...codex } })
       }
     },
     [ADD_NOTE] ({ commit }, payload) {
       const { note } = payload
       if (note) {
-        commit(STORE_NOTE, { note: {...note} })
+        commit(STORE_NOTE, { note: { ...note } })
+        commit(ATTACH_NOTE, { note: { ...note } })
       }
     },
     [EDIT_CODEX] ({ commit }, payload) {
       const { codex } = payload
       if (codex) {
         commit(UPDATE_CODEX, { codex: { ...codex } })
+        commit(ATTACH_CODEX, { codex: { ...codex } })
       }
     },
     [EDIT_NOTE] ({ commit }, payload) {
       const { note } = payload
       if (note) {
         commit(UPDATE_NOTE, { note: { ...note } })
+        commit(ATTACH_NOTE, { note: { ...note } })
       }
     },
     [DELETE_CODEX] ({ commit }, payload) {
       const { id } = payload
       if (id) {
         commit(DESTROY_CODEX, { id })
+        commit(DETACH_CODEX, { id })
       }
     },
     [DELETE_NOTE] ({ commit }, payload) {
       const { id } = payload
       if (id) {
         commit(DESTROY_NOTE, { id })
+        commit(DETACH_NOTE, { id })
       }
     }
   }
